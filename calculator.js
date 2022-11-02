e76ded41f7c8ad9c2b04b6553745bd490e70dc6a
ehsan <13008704+E-h-s-a-n@users.noreply.github.com>
2022-11-02 12:40:24 +0330

    all digits working

--- calculator.js
+++ calculator.js
@@ -1,46 +1,59 @@
-const add = (a, b) => {
-    return a + b
+function add(a, b) {
+    return a + b;
 }
 
-const subtract = (a, b) => {
-    return a - b
+function subtract (a, b) {
+    return a - b;
 }
 
-const multiply = (a, b) => {
-    return a * b
+function multiply(a, b) {
+    return a * b;
 }
 
 function divide(a, b) {
-    return a / b
+    return a / b;
 }
 
-const operate = (operator, operand1, operand2) => {
-
+const operate = (calcObj) => {
+    return window[`${calcObj.operation}`](+calcObj.first, +calcObj.second);
 }
 
 function display() {
     currentDisplay.textContent = displayValue;
 }
+
 let displayValue = '';
-let evaluation = { first: NaN, second: NaN, operation: '' }
+let calc = { first: '', second: '', operation: '' }
 
 const currentDisplay = document.querySelector("#display-1");
 const resultDisplay = document.querySelector("#display-2");
 const buttonsGrid = document.querySelector(".keys");
 
 buttonsGrid.addEventListener('click', e => {
-    const eCont = (s) => e.target.classList.contains(s);
+    const eClass = (s) => e.target.classList.contains(s);
+    const eValue = e.target.value;
 
-    if (eCont('number')) {
-        if (displayValue.length > 28) return
-        displayValue += e.target.value
-        display()
-    } else if (eCont('operate')) {
+    if (eClass('number')) {
+        if ((!calc.first || !calc.second) && eValue == '0') return;
+        if (calc.first.length > 25) return;
+        if (calc.operation){
+            calc.second += eValue;
+            displayValue = calc.second;
+        } else {
+            calc.first += eValue;
+            displayValue = calc.first;
+        }
+        display();
 
+    } else if (eClass('operate')) {
+        calc.operation = eValue;
+
+    } else if (eClass('equal')) {
+        operate(calc);
     }
 
-})
+});
 
 
-
-display()
+currentDisplay.textContent = 0;
+// resultDisplay.setAttribute('disabled');
