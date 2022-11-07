88a70d19023d438e65e5150157fb47feebfc07a4
ehsan <13008704+E-h-s-a-n@users.noreply.github.com>
2022-11-07 17:07:47 +0330

    Modify script

--- calculator.js
+++ calculator.js
@@ -18,8 +18,8 @@ const operate = (calcObj) => {
     return window[`${calcObj.operation}`](+calcObj.first, +calcObj.second);
 }
 
-function display() {
-    currentDisplay.textContent = displayValue;
+function display(value = '') {
+    currentDisplay.textContent = value;
 }
 
 let displayValue = '';
@@ -28,48 +28,50 @@ let calc = { first: '', operation: '', second: '' }
 const currentDisplay = document.querySelector("#display-1");
 const resultDisplay = document.querySelector("#display-2");
 const buttonsGrid = document.querySelector(".keys");
+console.clear();
 
 buttonsGrid.addEventListener('click', e => {
-    const eClass = (s) => e.target.classList.contains(s);
-    const eValue = e.target.value;
+    const keyType = e.target.dataset.type;
+    const keyValue = e.target.value;
 
-    if (eClass('number')) {
+    if (keyType == 'number') {
         if (calc.operation) {
-            if (!calc.second && eValue == '0' || calc.second.length > 19) return; //error
-            calc.second += eValue;
+            if (!calc.second && keyValue == '0' || calc.second.length > 19) return; //error
+            calc.second += keyValue;
             displayValue = calc.second;
+            display(calc.second);
         } else {
-            if (!calc.first && eValue == '0' || calc.first.length > 19) return; //error
-            calc.first += eValue;
+            if (!calc.first && keyValue == '0' || calc.first.length > 19) return; //error
+            calc.first += keyValue;
             displayValue = calc.first;
+            display(calc.first);
         }
-        display();
 
-    } else if (eClass('operate')) {
+
+    } else if (keyType == 'operator') {
         if (calc.second) {
             calc.first = operate(calc)
             calc.second = '';
-            calc.operation = '';
-            console.log(calc);
+            calc.operation = keyValue;
             displayValue = calc.first;
-            display();
+            display(calc.first);
         } else if (calc.first) {
-            calc.operation = eValue;
+            calc.operation = keyValue;
         }
 
-    } else if (eClass('equal')) {
+    } else if (keyType == 'equal') {
         if (!calc.second) return;
         displayValue = operate(calc);
         calc.first = '';
         calc.second = '';
         calc.operation = '';
-        display();
-    } else if (eClass('all-clear')) {
+        display(displayValue);
+    } else if (keyType == 'all-clear') {
         calc.first = '';
         calc.second = '';
         calc.operation = '';
         displayValue = ''
-        display()
+        display();
     }
     console.log(calc);
 
@@ -84,5 +86,6 @@ document.querySelector('.odin-click').addEventListener('click', e => {
     console.log(e.target.classList.toggle("odin"));
 })
 
-currentDisplay.textContent = '0';
+// currentDisplay.textContent = '0';
+display('0')
 // resultDisplay.setAttribute('disabled');
